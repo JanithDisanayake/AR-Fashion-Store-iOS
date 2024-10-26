fed0c1b5b6305b5774a2c91120f224174a6b5328
Janith Disanayake <janithdisanayake@users.noreply.github.com>
2024-10-26 22:51:20 +0530

    Initial commit
    Signed-off-by: Janith Disanayake <janithdisanayake@users.noreply.github.com>

---
+++ sarasboutique-ios/sarasboutique_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  sarasboutique_iosApp.swift
+//  sarasboutique-ios
+//
+//  Created by Janith Personal on 2024-11-01.
+//
+
+import SwiftUI
+
+@main
+struct sarasboutique_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sarasboutique-iosTests/sarasboutique_iosTests.swift
@@ -0,0 +1,16 @@
+//
+//  sarasboutique_iosTests.swift
+//  sarasboutique-iosTests
+//
+//  Created by Janith Personal on 2024-11-01.
+//
+
+import Testing
+
+struct sarasboutique_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
