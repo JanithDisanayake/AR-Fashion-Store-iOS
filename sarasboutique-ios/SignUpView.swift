3e1e388871e1610f94cce6facdc1834395970c40
Janith Disanayake <janithdisanayake@users.noreply.github.com>
2024-11-01 07:06:20 +0530

    implement signup page
    Signed-off-by: Janith Disanayake <janithdisanayake@users.noreply.github.com>

--- sarasboutique-ios/SignUpView.swift
+++ sarasboutique-ios/SignUpView.swift
@@ -8,8 +8,66 @@
 import SwiftUI
 
 struct SignUpView: View {
+    @State private var first_name = ""
+    @State private var last_name = ""
+    @State private var email = ""
+    @State private var password = ""
+    @State private var confirmPassword = ""
     var body: some View {
-        Text("Sign up !")
+        NavigationView {
+            VStack {
+                Text("Welcome to Saras Boutique!")
+                    .font(.title)
+                    .padding()
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                Spacer()
+                TextField("First Name", text: self.$first_name)
+                    .padding()
+                    .background(.ultraThinMaterial)
+                    .shadow(radius: 5)
+                    .cornerRadius(10)
+                TextField("Last Name", text: self.$last_name)
+                    .padding()
+                    .background(.ultraThinMaterial)
+                    .shadow(radius: 5)
+                    .cornerRadius(10)
+                TextField("Email", text: self.$email)
+                    .padding()
+                    .background(.ultraThinMaterial)
+                    .shadow(radius: 5)
+                    .cornerRadius(10)
+                SecureField("Password", text: self.$password)
+                    .padding()
+                    .background(.ultraThinMaterial)
+                    .shadow(radius: 5)
+                    .cornerRadius(10.0)
+                SecureField("Confirm Password", text: self.$confirmPassword)
+                    .padding()
+                    .background(.ultraThinMaterial)
+                    .shadow(radius: 5)
+                    .cornerRadius(10.0)
+                Spacer()
+                Text("Sign Up")
+                    .font(.system(size: 24, weight: .bold, design: .default))
+                    .frame(maxWidth: .infinity, maxHeight: 60)
+                    .foregroundColor(Color.white)
+                    .background(Color.blue)
+                    .cornerRadius(30)
+                HStack {
+                    Text("Already Have a Account")
+                    NavigationLink(
+                        destination:
+                            LoginView()
+                            .navigationBarBackButtonHidden(true)
+                    ) {
+                        Text("Login")
+                            .foregroundStyle(.blue)
+                    }
+                }
+            }
+            .padding(30)
+        }
     }
 }
 
