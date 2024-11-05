1a795ba06fcadfabe9cb68ebcb11f928404b930b
Janith Disanayake <janithdisanayake@users.noreply.github.com>
2024-11-05 23:46:09 +0530

    modify the login page
    Signed-off-by: Janith Disanayake <janithdisanayake@users.noreply.github.com>

--- sarasboutique-ios/LoginView.swift
+++ sarasboutique-ios/LoginView.swift
@@ -13,12 +13,12 @@ struct LoginView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Text("Welcome Back to Saras Boutique!")
+                Text("Welcome Back !")
                     .font(.title)
                     .padding()
                     .fontWeight(.bold)
                     .multilineTextAlignment(.center)
-                Spacer()
+                Image(.sarasbotiqueLogo)
                 TextField("Email", text: self.$email)
                     .padding()
                     .background(.ultraThinMaterial)
@@ -30,6 +30,16 @@ struct LoginView: View {
                     .shadow(radius: 5)
                     .cornerRadius(10.0)
                 Spacer()
+                Text("Forgot Password ?")
+                    .font(.system(size: 16))
+                    .foregroundColor(.blue)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .padding(.leading, 20)
+                    .font(.system(size: 16))
+                    .foregroundColor(.blue)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                Spacer()
+                    .frame(height:30)
                 NavigationLink(
                     destination:
                         MainView()
@@ -42,6 +52,77 @@ struct LoginView: View {
                         .background(Color.blue)
                         .cornerRadius(30)
                 }
+                Text("---------- or ----------")
+                NavigationLink(
+                    destination:
+                        MainView()
+                        .navigationBarBackButtonHidden(true)
+                ) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 30)
+                            .stroke(Color.gray, lineWidth: 2) // Set the border color and width here
+                        HStack(alignment: .center, spacing: 20) {
+                            Image(systemName: "apple.logo")
+                                .font(.title)
+                                .foregroundColor(.black)
+                            Text("Continue with Apple")
+                                .font(.system(size: 20, weight: .bold, design: .default))
+                                .foregroundColor(Color.black)
+                        }
+                        .font(.system(size: 16))
+                        .foregroundColor(.blue)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 40)
+                    }
+                    .frame(maxHeight: 50)
+                }
+                NavigationLink(
+                    destination:
+                        MainView()
+                        .navigationBarBackButtonHidden(true)
+                ) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 30)
+                            .stroke(Color.gray, lineWidth: 2) // Set the border color and width here
+                        HStack(alignment: .center, spacing: 20) {
+                            Image(.googleLogo)
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                            Text("Continue with Google")
+                                .font(.system(size: 20, weight: .bold, design: .default))
+                                .foregroundColor(Color.black)
+                        }
+                        .font(.system(size: 16))
+                        .foregroundColor(.blue)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 40)
+                    }
+                    .frame(maxHeight: 50)
+                }
+                NavigationLink(
+                    destination:
+                        MainView()
+                        .navigationBarBackButtonHidden(true)
+                ) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 30)
+                            .stroke(Color.gray, lineWidth: 2) // Set the border color and width here
+                        HStack(alignment: .center, spacing: 20) {
+                            Image(.facebookLogo)
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                            Text("Continue with Facebook")
+                                .font(.system(size: 20, weight: .bold, design: .default))
+                                .foregroundColor(Color.black)
+                        }
+                        .font(.system(size: 16))
+                        .foregroundColor(.blue)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 40)
+                    }
+                    .frame(maxHeight: 50)
+                }
+                
                 HStack {
                     Text("Don't Have a Account")
                     NavigationLink(
@@ -54,7 +135,7 @@ struct LoginView: View {
                     }
                 }
             }
-            .padding(30)
+            .padding(.horizontal, 30)
         }
     }
 }
