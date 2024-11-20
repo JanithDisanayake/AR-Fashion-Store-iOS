1502952d5675145164bafa2719921668d8569f7f
Janith Disanayake <janithdisanayake@users.noreply.github.com>
2024-11-20 15:44:49 +0530

    fix product detaild view ui
    Signed-off-by: Janith Disanayake <janithdisanayake@users.noreply.github.com>

--- sarasboutique-ios/Sources/Views/ProductDetailsView.swift
+++ sarasboutique-ios/Sources/Views/ProductDetailsView.swift
@@ -9,110 +9,91 @@ import SwiftUI
 
 struct ProductDetailsView: View {
     var body: some View {
-        ZStack() {
-            
-            ZStack() {
-                Rectangle()
-                    .foregroundColor(.clear)
-                    .frame(width: 440, height: 600)
-                    .background(Color(red: 0.77, green: 0.77, blue: 0.77))
-                    .cornerRadius(20)
-                    .offset(x: 0, y: 0)
-                    .shadow(
-                        color: Color(red: 0, green: 0, blue: 0, opacity: 0.10), radius: 40, y: 4
-                    )
-                Rectangle()
-                    .foregroundColor(.clear)
-                    .frame(width: 440, height: 600)
-                    .background(
-                        AsyncImage(url: URL(string: "https://via.placeholder.com/440x567"))
-                    )
-                    .cornerRadius(20)
-                    .offset(x: 0, y: 0)
-            }
-            .frame(width: 440, height: 567)
-            .offset(x: 0, y: -200)
-            
-            VStack {
-                VStack() {
-                    
-                    VStack {
-                        HStack{
-                            VStack{
-                                Text("Elegant Blazer")
-                                    .font(Font.custom("Poppins", size: 20).weight(.semibold))
-                                    .foregroundColor(Color(red: 0.21, green: 0.21, blue: 0.21))
-                                    .multilineTextAlignment(.leading)
-                                    .frame(maxWidth: .infinity)
-                                    .padding(2)
-                                Text("Women Style")
-                                    .font(Font.custom("Poppins", size: 10).weight(.medium))
-                                    .foregroundColor(Color(red: 0.21, green: 0.21, blue: 0.21))
-                                    .multilineTextAlignment(.leading)
-                                    .opacity(0.50)
-                                    .frame(width: .infinity)
-                            }
-                                
-                            Text("$129,99")
-                                .font(Font.custom("Poppins", size: 20).weight(.semibold))
-                                .foregroundColor(Color(red: 0.21, green: 0.21, blue: 0.21))
-                                .multilineTextAlignment(.trailing)
-                                .frame(maxWidth: .infinity)
-                                
-                        }
+        VStack() {
+            ScrollView(.vertical) {
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(.gray)
+                        .frame(width: .infinity, height: 500)
+
+                    AsyncImage(url: URL(string: "https://www.saras-boutique.com/_app/immutable/assets/Prod1.B56R4Eth.jpg"))
                         .padding()
-                        
-                    }
-                    
-                    ZStack() {
-                        Text("Color")
-                            .font(Font.custom("Poppins", size: 12).weight(.medium))
-                            .foregroundColor(.black)
-                            .offset(x: -180.53, y: 0.56)
-                        Ellipse()
-                            .foregroundColor(.clear)
-                            .frame(width: 33.04, height: 32.52)
-                            .background(Color(red: 0.59, green: 0.26, blue: 0.02))
-                            .offset(x: 183.48, y: 0)
-                        Ellipse()
-                            .foregroundColor(.clear)
-                            .frame(width: 33.04, height: 32.52)
-                            .background(Color(red: 0.07, green: 0, blue: 0.84))
-                            .offset(x: 138.64, y: 0)
-                        Ellipse()
-                            .foregroundColor(.clear)
-                            .frame(width: 33.04, height: 32.52)
-                            .background(Color(red: 0.13, green: 0.13, blue: 0.13))
-                            .offset(x: 93.81, y: 0)
-                        Ellipse()
-                            .foregroundColor(.clear)
-                            .frame(width: 38.94, height: 37)
-                            .background(Color(red: 0.84, green: 0, blue: 0))
-                            .offset(x: 46.02, y: 0)
-                    }
-                    .frame(width: 400, height: 37)
-                    .offset(y:10)
-                    
-                    VStack() {
-                        Text("Description")
-                            .font(Font.custom("Poppins", size: 12).weight(.medium))
-                            .foregroundColor(Color(red: 0.21, green: 0.21, blue: 0.21))
-                            .padding()
-                        Text("Made from premium fabrics that exude luxury, such as fine wool, soft cashmere, or smooth silk blends, the blazer not only radiates elegance but also ensures comfort and durability. Its thoughtful design features often include a single-breasted closure with tasteful buttons, creating a focal point that complements the blazer's overall sophistication.")
-                            .font(Font.custom("Poppins", size: 10).weight(.medium))
-                            .multilineTextAlignment(.leading)
-                            .foregroundColor(Color(red: 0.21, green: 0.21, blue: 0.21))
-                            .opacity(0.70)
-                    }
-                    .frame(height: 103)
+                        .cornerRadius(50)
                 }
-                .frame(width: 400, height: 45)
-                .offset(x: 0, y: 220)
+                .shadow(color: .gray.opacity(0.5), radius: 5, x: 0, y: 5)
                 
+                HStack{
+                    VStack{
+                        Text("Elegant Blazer")
+                            .font(Font.custom("Poppins", size: 20).weight(.semibold))
+                            .foregroundColor(Color(red: 0.21, green: 0.21, blue: 0.21))
+                            .multilineTextAlignment(.leading)
+                            .frame(maxWidth: .infinity)
+                            .padding(2)
+                        Text("Women Style")
+                            .font(Font.custom("Poppins", size: 10).weight(.medium))
+                            .foregroundColor(Color(red: 0.21, green: 0.21, blue: 0.21))
+                            .multilineTextAlignment(.leading)
+                            .opacity(0.50)
+                            .frame(width: .infinity)
+                    }
+
+                    Text("$129,99")
+                        .font(Font.custom("Poppins", size: 20).weight(.semibold))
+                        .foregroundColor(Color(red: 0.21, green: 0.21, blue: 0.21))
+                        .multilineTextAlignment(.trailing)
+                        .frame(maxWidth: .infinity)
+
+                }
+                .padding()
+
+                HStack {
+//                    Text("Color")
+//                        .font(Font.custom("Poppins", size: 12).weight(.medium))
+//                        .foregroundColor(.black)
+
+                    HStack(spacing: 8) { 
+                        Ellipse()
+                            .foregroundColor(.clear)
+                            .frame(width: 32, height: 32)
+                            .background(Color(red: 0.59, green: 0.26, blue: 0.02))
+                            .background(Color(hex: "#592602"))
+                            .cornerRadius(10)
+                        Ellipse()
+                            .foregroundColor(.clear)
+                            .frame(width: 32, height: 32)
+                            .background(Color(hex: "#0707FF"))
+                            .cornerRadius(10)
+                        Ellipse()
+                            .foregroundColor(.clear)
+                            .frame(width: 32, height: 32)
+                            .background(Color(hex: "#131313"))
+                            .cornerRadius(10)
+                        Ellipse()
+                            .foregroundColor(.clear)
+                            .frame(width: 32, height: 32)
+                            .background(Color(hex: "#D80000"))
+                            .cornerRadius(10)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
                 
-                
+                VStack() {
+                    Text("Description")
+                        .font(Font.custom("Poppins", size: 16).weight(.bold))
+                        .foregroundColor(Color(red: 0.21, green: 0.21, blue: 0.21))
+                        .padding()
+                    Text("Made from premium fabrics that exude luxury, such as fine wool, soft cashmere, or smooth silk blends, the blazer not only radiates elegance but also ensures comfort and durability. Its thoughtful design features often include a single-breasted closure with tasteful buttons, creating a focal point that complements the blazer's overall sophistication.")
+                        .font(Font.custom("Poppins", size: 10).weight(.medium))
+                        .multilineTextAlignment(.leading)
+                        .foregroundColor(Color(red: 0.21, green: 0.21, blue: 0.21))
+                        .opacity(0.70)
+                }
+                .padding(.horizontal, 50)
             }
-            .padding(100)
+            .padding(10)
+            .frame(width: .infinity, height: .infinity)
             
             HStack(alignment: .top, spacing: 18) {
                 ZStack() {
@@ -140,9 +121,9 @@ struct ProductDetailsView: View {
                 }
                 .frame(width: 266, height: 55)
             }
-            .offset(x: -0.50, y: 403.50)
+            .padding()
         }
-        .frame(width: 440, height: 956)
+        .frame(width: 440, height: .infinity)
         .background(.white)
     }
 }
--- sarasboutique-ios/sarasboutique_iosApp.swift
+++ sarasboutique-ios/sarasboutique_iosApp.swift
@@ -25,7 +25,7 @@ struct sarasboutique_iosApp: App {
     
     var body: some Scene {
         WindowGroup {
-            LoginView()
+            ProductDetailsView()
         }
     }
 }
