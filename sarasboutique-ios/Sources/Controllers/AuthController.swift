ff425e706c2fcddf21f01e592638ffbf7d060340
Janith Disanayake <janithdisanayake@users.noreply.github.com>
2024-11-21 21:46:30 +0530

    sign up user through google
    Signed-off-by: Janith Disanayake <janithdisanayake@users.noreply.github.com>

--- sarasboutique-ios/Sources/Controllers/AuthController.swift
+++ sarasboutique-ios/Sources/Controllers/AuthController.swift
@@ -11,6 +11,8 @@ import GoogleSignIn
 import GoogleSignInSwift
 
 class AuthController : ObservableObject {
+    private let userController = UserController()
+    
     func handleSignInButton(completion: @escaping (Bool) -> Void) {
         guard let presentingViewController = (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.windows.first?.rootViewController else {
             completion(false)
@@ -26,6 +28,43 @@ class AuthController : ObservableObject {
                 // Inspect error if needed
                 print("Error during sign in:", error?.localizedDescription ?? "Unknown error")
                 // Call the completion handler with false to indicate failure
+                completion(false)
+            }
+        }
+    }
+    
+    func handleSignUpButton(completion: @escaping (Bool) -> Void) {
+        guard let presentingViewController = (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.windows.first?.rootViewController else {
+            completion(false)
+            return
+        }
+
+        GIDSignIn.sharedInstance.signIn(withPresenting: presentingViewController) { signInResult, error in
+            if let result = signInResult {
+                print("Sign In Successful")
+
+                if let userProfile = result.user.profile {
+                    let firstName = userProfile.givenName ?? "No first name"
+                    let lastName = userProfile.familyName ?? "No last name"
+                    let email = result.user.profile?.email ?? "No email"
+                    
+                    print("User \(firstName) \(lastName) \(email)")
+                    var user = User(userId: "", firstName: firstName, lastName: lastName, email: email)
+                    
+                    self.userController.register(user: user) { result in
+                        DispatchQueue.main.async {
+                            switch result {
+                            case .success:
+                                print("User successfully registered")
+                            case .failure(let error):
+                                print("Error registering user: \(error.localizedDescription)")
+                            }
+                        }
+                    }
+                }
+                completion(true)
+            } else {
+                print("Error during sign in:", error?.localizedDescription ?? "Unknown error")
                 completion(false)
             }
         }
